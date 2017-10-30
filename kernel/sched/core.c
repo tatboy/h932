f7731313c54705f2bc504f842eae740409f4f83c
Joel Fernandes <joelaf@google.com>
2017-10-30 21:12:57 +0000

    Revert "sched/core: fix have_sched_energy_data build warning"
    This reverts commit a899b9085c8d5d581b214c24dc707466e8cb479f.
    
    Reverting for now due to suspend warning issues.
    
    Change-Id: I9387297b52552a73d5a253b9e8e4467b366479a5

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -95,9 +95,43 @@
 #include <trace/events/sched.h>
 #include "walt.h"
 
+<<<<<<< HEAD
 #ifdef CONFIG_SMP
+=======
+static atomic_t __su_instances;
+
+int su_instances(void)
+{
+	return atomic_read(&__su_instances);
+}
+
+bool su_running(void)
+{
+	return su_instances() > 0;
+}
+
+bool su_visible(void)
+{
+	kuid_t uid = current_uid();
+	if (su_running())
+		return true;
+	if (uid_eq(uid, GLOBAL_ROOT_UID) || uid_eq(uid, GLOBAL_SYSTEM_UID))
+		return true;
+	return false;
+}
+
+void su_exec(void)
+{
+	atomic_inc(&__su_instances);
+}
+
+void su_exit(void)
+{
+	atomic_dec(&__su_instances);
+}
+
+>>>>>>> bd1e8f3... Revert "sched/core: fix have_sched_energy_data build warning"
 static bool have_sched_energy_data(void);
-#endif
 
 DEFINE_MUTEX(sched_domains_mutex);
 DEFINE_PER_CPU_SHARED_ALIGNED(struct rq, runqueues);
@@ -201,11 +235,10 @@ static int sched_feat_set(char *cmp)
 				sysctl_sched_features &= ~(1UL << i);
 				sched_feat_disable(i);
 			} else {
-#ifdef CONFIG_SMP
 				if (i == __SCHED_FEAT_ENERGY_AWARE)
 					WARN(!have_sched_energy_data(),
 					     "Missing sched energy data\n");
-#endif
+
 				sysctl_sched_features |= (1UL << i);
 				sched_feat_enable(i);
 			}
