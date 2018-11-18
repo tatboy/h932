08d657f8264f89dacab5419bb50ab5709566029d
Brett Sharp <tattooboy12@gmail.com>
2018-11-17 21:37:38 -0800

    Shit

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#0 SMP PREEMPT Sat Nov 17 19:58:50 PST 2018"
+#define LINUX_COMPILE_BY "tatz"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILER "gcc version 4.9.4 (Linaro GCC 4.9-2017.01) "
