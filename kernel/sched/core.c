3110cfafb68f9048ecd631be29d0f0c63797248f
Brett Sharp <tattooboy12@gmail.com>
2018-11-17 18:24:02 -0800

    OMG

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -95,42 +95,6 @@
 #include <trace/events/sched.h>
 #include "walt.h"
 
-<<<<<<< HEAD
-static bool have_sched_energy_data(void);
-=======
-static atomic_t __su_instances;
-
-int su_instances(void)
-{
-	return atomic_read(&__su_instances);
-}
-
-bool su_running(void)
-{
-	return su_instances() > 0;
-}
-
-bool su_visible(void)
-{
-	kuid_t uid = current_uid();
-	if (su_running())
-		return true;
-	if (uid_eq(uid, GLOBAL_ROOT_UID) || uid_eq(uid, GLOBAL_SYSTEM_UID))
-		return true;
-	return false;
-}
-
-void su_exec(void)
-{
-	atomic_inc(&__su_instances);
-}
-
-void su_exit(void)
-{
-	atomic_dec(&__su_instances);
-}
->>>>>>> 5078fdf... Revert "sched/core: Warn if ENERGY_AWARE is enabled but data is missing"
-
 DEFINE_MUTEX(sched_domains_mutex);
 DEFINE_PER_CPU_SHARED_ALIGNED(struct rq, runqueues);
 
